f7246b1dbedbb4b98636c3cff7b4e74a1814f816
Ar1f007 <muhammadarif9416@yahoo.com>
2022-03-29 21:26:42 +0600

    fade in fade out animation to feedback item

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,25 @@
 import { FeedbackItem } from './';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) return <p>No Feed yet</p>;
 
   return (
     <div className="feedback-list">
-      {feedback?.map((item) => {
-        return <FeedbackItem key={item.id} {...item} handleDelete={handleDelete} />;
-      })}
+      <AnimatePresence>
+        {feedback?.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem {...item} handleDelete={handleDelete} />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 };
